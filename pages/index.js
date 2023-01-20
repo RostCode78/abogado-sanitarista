d6ef83490447707894a250fcb348940ebdfe20bd
RostCode78 <zeroxcode78@gmail.com>
2023-01-20 00:59:41 -0600

    Feat: Seccion Jurista

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from "../components/PaginaInicio/Header/Header";
 import Servicios from "../components/PaginaInicio/Servicios/Servicios";
 import ServicioEspecial from "../components/PaginaInicio/ServicioEspecial/ServicioEspecial";
 import Comunidad from "../components/PaginaInicio/Comunidad/Comunidad";
+import Jurista from "../components/PaginaInicio/Jurista/Jurista";
 
 const index = () => {
   return (
@@ -12,6 +13,7 @@ const index = () => {
             <Servicios/>
             <ServicioEspecial/>
             <Comunidad/>
+            <Jurista/>
         </div>
     </Layout>
   )
